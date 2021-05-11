a5ff5c083c05231322b59b44095859e47861e48c
Alfredo Rivas Jimenez <alfredo_rivas_jimenez@outlook.com>
2021-05-11 08:05:12 -0500

    About me

--- js/headerFunctions.js
+++ js/headerFunctions.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.toggle-button');
 const navMenu = document.querySelector('.nav_menu');
+const closeNave = document.querySelector('#close_nav');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('nav_menu_visible');
@@ -8,4 +9,11 @@ navToggle.addEventListener('click', () => {
     }else{
         navToggle.setAttribute("aria-laber", "Open menu")
     }
+})
+
+closeNave.addEventListener('click',() =>{
+    if (navMenu.classList.contains("nav_menu_visible")) {
+        navMenu.classList.remove("nav_menu_visible")
+    }
+    
 })
